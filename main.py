938caa3e06e18b719097164ae1f17742168ea347
KJ O'BRIEN <kjokjo219@gmail.com>
2022-08-15 11:45:05 -0400

    Modulizers and cleans up code. Product is working now

--- main.py
+++ main.py
@@ -1,42 +1,21 @@
 from moviepy.editor import *
 import moviepy
 
-def createSample():
-    clip = VideoFileClip("curb.mp4").subclip(0,20)
-
-    clip = clip.volumex(0.8)
-
-    txt_clip = TextClip("Curb Song",fontsize=70,color='white')
-
-    # Say that you want it to appear 10s at the center of the screen
-    txt_clip = txt_clip.set_pos('center').set_duration(10)
-
-    # Overlay the text clip on the first video clip
-    video = CompositeVideoClip([clip, txt_clip])
-
-    # Write the result to a file (many options available !)
-    video.write_videofile("curbwithtext.webm")
-
-def convertBPMToSeconds(bpm):
+def convert_bpm_to_seconds(bpm):
     return 60.0/bpm
 
-
+input = 6
+tempo = 120
 audio = AudioFileClip(("120bpmtempo.wav"))
-img = ImageClip("1.jpg", duration = convertBPMToSeconds(15))
-img2 = ImageClip("1.jpg", duration = convertBPMToSeconds(15))
+finalclips = []
 
-#tester = VideoFileClip("1.jpg", duration = convertBPMToSeconds(15))
-tester = VideoFileClip("1.jpg")
-tester2 = VideoFileClip("2.jpg")
-tester3 = VideoFileClip("3.jpg")
+for i in range(1, input+1, 1):
+    temp = VideoFileClip(str(str(i) + ".jpg"))
+    pos = convert_bpm_to_seconds(tempo)
+    temp = temp.set_duration(pos)
+    temp = temp.set_start((i-1)*pos)
+    finalclips.append(temp)
 
-tester = tester.set_duration(convertBPMToSeconds(15))
-tester2 = tester2.set_duration(convertBPMToSeconds(15))
-tester3 = tester3.set_duration(convertBPMToSeconds(15))
-
-
-dexter = CompositeVideoClip([tester, tester2.set_start(convertBPMToSeconds(120)), tester3.set_start(2*convertBPMToSeconds(120))])
-
-dexter.write_videofile("onbeat.mp4", fps=24)
-
-print(convertBPMToSeconds(120))
+dexter = CompositeVideoClip(finalclips)
+dexter = dexter.set_audio(audio)
+dexter.write_videofile("final.webm", fps=24)
