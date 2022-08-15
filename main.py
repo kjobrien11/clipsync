2df54a1ed504213d2d99bbd13ce0f919b62c562f
KJ O'BRIEN <kjokjo219@gmail.com>
2022-08-15 13:53:10 -0400

    Adds GUI input support to correctly accept user input and have that take effect in the results folder

--- main.py
+++ main.py
@@ -1,23 +1,40 @@
 from moviepy.editor import *
 import moviepy
+import PySimpleGUI as sg
 
+speed = {"Two Beats": 2, "Beat": 1, "Half Beat": 0.5, "Quarter Beat": 0.25}
 
-speed = {"two beats": 2, "beat": 1, "half beat": 0.5, "quarter beat": 0.25}
+layout = [
+      [sg.Text('Welcome')],
+      [sg.Text('Time Division:', size=(20, 1)), sg.Combo(['Two Beats', 'Beat', "Half Beat", "Quarter Beat"])],
+      [sg.Text('BPM:', size=(20, 1)), sg.InputText()],
+      [sg.Text('Song Start Time (sec):', size=(20, 1)), sg.InputText()],
+      [sg.Text('Picture Folder', size=(20, 1)), sg.InputText(), sg.FolderBrowse()],
+      [sg.Text('File of Instrumental', size=(20, 1)), sg.InputText(), sg.FileBrowse()],
+      [sg.Submit(), sg.Cancel()]]
+
+window = sg.Window('Rename Files or Folders', layout)
+
+event, values = window.read()
+window.close()
+folder_path, file_path = values[0], values[1]       # get the data from the values dictionary
 
 #convert bpm to seconds
-def convert_bpm_to_seconds(bpm, divsion = "beat"):
+def convert_bpm_to_seconds(bpm, divsion = "Beat"):
     return 60.0/(bpm/speed[divsion])
 
-#initial vars
-input = 11
-tempo = 105
-seconds = convert_bpm_to_seconds(tempo, "quarter beat")
-start_time = 9.35
-end_time = start_time + input * convert_bpm_to_seconds(tempo)
-audio = AudioFileClip(("105_mind_right.mp3"))
-audio = audio.subclip(start_time, end_time)
-final_clips = []
+#provided by user
+input = 11 #how many total pictures
+division = values[0]
+tempo = float(values[1]) #bpm of the beat provided by user
+start_time = float(values[2]) #where in the song they want the pictures to start
+audio = AudioFileClip(("105_mind_right.mp3")) #mp3 or wav file of the song
 
+#computed based on input
+seconds = convert_bpm_to_seconds(tempo, division) #conversion of bpm to seconds for display use
+end_time = start_time + input * seconds #when to end the video to ensure no excess
+audio = audio.subclip(start_time, end_time) #sets the correct start & end times for the beat
+final_clips = [] #stores indivual VideoFileClip to be used to combine at the end
 
 #prepare the clips for video
 for i in range(1, input+1, 1):
@@ -30,4 +47,4 @@ for i in range(1, input+1, 1):
 #combine clips and export
 final = CompositeVideoClip(final_clips)
 final = final.set_audio(audio)
-final.write_videofile("quar.webm", fps=24)
+final.write_videofile("final.webm", fps=24)
