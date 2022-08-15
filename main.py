0155514e93f05fab4a77978b322bde77c58bc0b9
KJ O'BRIEN <kjokjo219@gmail.com>
2022-08-15 12:09:39 -0400

    Adds new files, makes the start and end times variable and sets the video to end after the last clip

--- main.py
+++ main.py
@@ -4,18 +4,22 @@ import moviepy
 def convert_bpm_to_seconds(bpm):
     return 60.0/bpm
 
-input = 6
-tempo = 120
-audio = AudioFileClip(("120bpmtempo.wav"))
-finalclips = []
+
+input = 11
+tempo = 128
+start_time = 0
+end_time = input * convert_bpm_to_seconds(tempo)
+audio = AudioFileClip(("128_new_choppa.mp3"))
+audio = audio.subclip(start_time, end_time)
+final_clips = []
 
 for i in range(1, input+1, 1):
     temp = VideoFileClip(str(str(i) + ".jpg"))
     pos = convert_bpm_to_seconds(tempo)
     temp = temp.set_duration(pos)
     temp = temp.set_start((i-1)*pos)
-    finalclips.append(temp)
+    final_clips.append(temp)
 
-dexter = CompositeVideoClip(finalclips)
-dexter = dexter.set_audio(audio)
-dexter.write_videofile("final.webm", fps=24)
+final = CompositeVideoClip(final_clips)
+final = final.set_audio(audio)
+final.write_videofile("final.webm", fps=24)
